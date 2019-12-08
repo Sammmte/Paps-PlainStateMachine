9af9ea03e24bd29915f84b6b1b98e6a1f454d162
Santiago Pérez <santiagoperez.-@hotmail.com>
2019-12-08 16:53:10 -0300

    updated README.md

--- FSM/Assets/Package/FSM/Extensions/EmptyState.cs
+++ FSM/Assets/Package/FSM/Extensions/EmptyState.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace Paps.FSM.Extensions
+﻿namespace Paps.FSM.Extensions
 {
     public class EmptyState : IState
     {
